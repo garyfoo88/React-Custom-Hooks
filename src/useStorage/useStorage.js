8356cf0b757d8c5c9d4c8ee3adf19fdab1f5a763
garyfoo88 <reid9000200020002000@gmail.com>
2021-09-16 20:53:16 +0800

    added logic for use storage

--- src/useStorage/useStorage.js
+++ src/useStorage/useStorage.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import { useCallback, useState, useEffect } from "react"
 
-function useStorage() {
-    return (
-        <div>
-            Use storage logic here!!
-        </div>
-    )
+export function useLocalStorage(key, defaultValue) {
+  return useStorage(key, defaultValue, window.localStorage)
 }
 
-export default useStorage
+export function useSessionStorage(key, defaultValue) {
+  return useStorage(key, defaultValue, window.sessionStorage)
+}
+
+function useStorage(key, defaultValue, storageObject) {
+  const [value, setValue] = useState(() => {
+    const jsonValue = storageObject.getItem(key)
+    if (jsonValue != null) return JSON.parse(jsonValue)
+
+    if (typeof initialValue === "function") {
+      return defaultValue()
+    } else {
+      return defaultValue
+    }
+  })
+
+  useEffect(() => {
+    if (value === undefined) return storageObject.removeItem(key)
+    storageObject.setItem(key, JSON.stringify(value))
+  }, [key, value, storageObject])
+
+  const remove = useCallback(() => {
+    setValue(undefined)
+  }, [])
+
+  return [value, setValue, remove]
+}
